c8c96cf85f95ce315cdadc57e4ddb9a53f2689d3
Yasiru Swaris <52125232+Yasiruofficial@users.noreply.github.com>
2023-02-13 23:27:06 +0530

    Chat animation Done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,92 @@
 <template>
   <v-container>
-      <v-row style="height: 500px;">
-        <v-col>
-          <v-sheet class="pa-2 rounded-lg" elevation="12">
-            <div class="chat-container">
-              <div class="chat-header">
-                <h3>Finetune AI Bot</h3>
-              </div>
-              <div class="chat-messages">
-                <ul>
-                  <li v-for="message in messages" :key="message.id" :style="{
-                    backgroundColor: message.type === 'A' ? '#FCF6F5FF' : '#FAEBEFFF',
-                    textAlign : message.type === 'A' ? 'left' : 'right'
-                    }">
-                    <p>{{ message.text }}</p>
-                  </li>
-                </ul>
-              </div>
-              <div class="chat-input">
-                <input type="text" v-model="newMessageText" @keyup.enter="addMessage" />
-                <button @click="addMessage">Send</button>
-              </div>
+    <v-row style="height: 500px;" justify="center">
+      <v-col cols="8" >
+        <v-sheet class="pa-2 rounded-lg" elevation="12">
+
+          <v-card elevation="4">
+            <div class="pa-4 text-center">
+              <h3>{{ selectedOption }} <v-icon color="green">mdi-account-badge-outline</v-icon></h3>
             </div>
-          </v-sheet>
-        </v-col>
-      </v-row>
+          </v-card>
+
+          <div class="chat-messages">
+            <v-row v-for="message in messages" :key="message.id" :justify="message.type === 'A' ? 'start' : 'end'" no-gutters>
+              <div class="pa-3 mt-3 rounded-xl sm-12" :id="'chat-' + message.id" round="4" :style="{
+                textAlign: message.type === 'A' ? 'start' : 'end',
+                backgroundColor: message.type === 'A' ? '#FCF6F5FF' : '#FAEBEFFF'
+              }"> <v-icon v-if="message.type === 'A'" color="green">mdi-account-badge-outline</v-icon> {{
+  message.text
+}} <v-icon v-if="message.type !== 'A'" color="green">mdi-chat</v-icon> </div>
+            </v-row>
+          </div>
+          <div>
+            <v-row class="pt-5" align="center">
+              <v-col cols="11"><v-text-field variant="outlined" label="Type here" hide-details v-model="newMessageText"
+                  required></v-text-field>
+              </v-col>
+              <v-col class="ml-5">
+                <v-btn @click="getData" icon="mdi-send" color="primary"></v-btn>
+              </v-col>
+            </v-row>
+            <v-row class="pb-5">
+              <v-col cols="6">
+                <v-select v-model="selectedOption" variant="outlined" :items="options" hide-details required
+                  label="Choose the model"></v-select>
+              </v-col>
+            </v-row>
+          </div>
+
+        </v-sheet>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+
+import axios from 'axios';
+import anime from 'animejs/lib/anime.es.js';
+
 export default {
   data() {
     return {
       messages: [
-        { id: 1, type:"A", text: "Hello!" },
-        { id: 2, type:"A", text: "How are you?" },
-        { id: 3, type:"Q", text: "I'm good, thanks for asking." }
+        { id: 1, type: "A", text: "Hello!" },
+        { id: 2, type: "Q", text: "I'm good, thanks for asking." }
       ],
-      newMessageText: ""
+      options: ["Finetune AI Bot", "Base Model"],
+      selectedOption: "Finetune AI Bot",
+      newMessageText: ''
     };
   },
   methods: {
     addMessage() {
+
       this.messages.push({
         id: this.messages.length + 1,
-        text: this.newMessageText
+        text: this.newMessageText,
+        type: "Q"
       });
+
       this.newMessageText = "";
+    },
+    async getData() {
+
+      await this.addMessage();
+
+      anime({
+        targets: '#chat-' + this.messages.length,
+        scale: [0, 1],
+        translateY: [100, 0],
+        duration: 150,
+        easing: 'easeOutQuad',
+        autoplay: true
+      });
+
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+
     }
   }
 };
@@ -54,47 +94,31 @@ export default {
 
 
 <style>
-/* *{
-  border:1px solid black;
-} */
-.chat-container {
-  width: 100%;
-  margin: 0 auto;
-}
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
 
-.chat-header {
-  background-color: #5B84B1FF;
-  padding: 10px;
-  text-align: center;
+* {
+  font-family: 'Poppins', sans-serif;
 }
 
 .chat-messages {
   background-color: #9CC3D5FF;
-  height: 70vh;
+  height: 60vh;
   overflow-y: scroll;
   padding: 10px;
 }
 
-.chat-messages ul li{
-  margin-bottom: 5px;;
+
+.chat-messages::-webkit-scrollbar {
+  width: 10px;
+  background-color: #c9c9c9;
 }
 
-.chat-input {
-  background-color: #5B84B1FF;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
+.chat-messages::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #467fa0;
 }
 
-.chat-input input[type="text"] {
-  flex: 1;
-  padding: 5px;
-  background-color: #9CC3D5FF;
-}
 
-.chat-input button {
-  padding: 5px 10px;
-  background-color: #9CC3D5FF;
-  margin-left: 5px;
-}
+
+
 </style>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -2,7 +2,7 @@
   <v-app-bar flat>
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-6" />
-      Essentials Preset
+      Chat bot
     </v-app-bar-title>
   </v-app-bar>
 </template>
