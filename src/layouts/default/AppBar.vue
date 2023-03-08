d726a551baff22b44ac8c5bc139e8c36b8e415d0
Yasiru Lakshan <59885914+yasirulakshan@users.noreply.github.com>
2023-03-08 13:36:09 +0530

    Logo Removed

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-img src="../../assets/rambase.png" width="200px"  />
+      <!-- <v-img src="../../assets/rambase.png" width="200px"  /> -->
+      <h1>RAMBASE</h1>
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
-<script setup>
-  
-</script>
+<script setup></script>
